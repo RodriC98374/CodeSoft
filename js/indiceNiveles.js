b3050c8f8ea1ac84d98bd1e5fcce3a3658da0a44
DafneXCA <cruzdafne123@gmail.com>
2021-11-12 20:26:27 -0400

    fix conexion índice

--- js/indiceNiveles.js
+++ js/indiceNiveles.js
@@ -1,15 +1,17 @@
 var contTemas=document.getElementsByClassName("TemaC");
 var indtemas=document.getElementsByClassName("Tema");
-setTimeout(()=>{añadirSubtemas()},2000);
+setTimeout(()=>{añadirSubtemas()},3000);
+console.log(":VVVVVVVVV");
 function añadirSubtemas(){
+    console.log(":VVVVVVVVV");
     for(var i=0; i<contTemas.length;i++){
         var divInd=document.createElement("div");
         var contSubtemas=contTemas[i].getElementsByClassName("Subtitulo");
-        //console.log(":VVVVVVVVV");
+        console.log(":VVVVVVVVV");
         //console.log(contTemas[i]);
         //console.log(":VVVVVVVVV");
         for(var j=0;j<contSubtemas.length;j++){
-           // console.log("xxxxxxx");
+           console.log("xxxxxxx");
             var subt= document.createElement("h5");
             subt.innerHTML="<a href='#"+contSubtemas[j].firstChild.innerHTML+"'>"+contSubtemas[j].firstChild.innerHTML+"</a>";
             subt.title=contSubtemas[j].firstChild.innerHTML;
